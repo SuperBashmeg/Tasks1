baa1c7fb6d84fcff114510b20cefc367a386fa38
Bashmeg <superbashmeg@gmail.com>
2024-05-13 19:52:30 +0300

    lab task 1 done

--- main.cpp
+++ main.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 
 int main() {
-
+	int a, b, temp, num;
+	cout << "Enter range limits: ";
+	cin >> a >> b;
+	if (b < a) {
+		temp = a;
+		a = b;
+		b = temp;
+	}
+	while (true)
+	{
+		cout << "Enter number: ";
+		cin >> num;
+		if (a <= num && num <= b) {
+			cout << "Correct Number!!!" << endl;
+			break;
+		}
+		else
+		{
+			cout << "YOU ARE STUPID IDIOT ENTER NUMBER WHAT INCLUDED IN RANGE!!!" << endl;
+		}
+	}
 	return 0;
 }
