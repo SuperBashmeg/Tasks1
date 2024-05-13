0fd41f127a7c2623784fed869dd3cdb33e861bec
Bashmeg <superbashmeg@gmail.com>
2024-05-13 19:17:34 +0300

    task 1_a done

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main() {
-
+	for (size_t i = 0; i < 5; i++)
+	{
+		for (size_t j = 0; j < 5; j++)
+		{
+			if (i > j) {
+				cout << "* ";
+			}
+			else
+			{
+				cout << "- ";
+			}
+			
+		}
+		cout << endl;
+	}
 	return 0;
 }
