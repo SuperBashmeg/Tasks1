5c92ff23e78d9591203e0fa05a935d16904d02bd
Bashmeg <superbashmeg@gmail.com>
2024-05-15 19:19:48 +0300

    hw task 2 finally done

--- main.cpp
+++ main.cpp
@@ -3,22 +3,32 @@ using namespace std;
 
 int main() {
 	const int SIZE = 12;
+	int firstMonth, lastMonth;
 	int income[SIZE];
 	for (size_t i = 0; i < SIZE; i++)
 	{
 		cout << "Enter income for " << i + 1 << " month: ";
 		cin >> income[i];
 	}
-	int max = income[0], min = income[0];
-	for (size_t i = 0; i < SIZE; i++)
+	do
 	{
-		if (income[i] > max) {
-			max = income[i];
-		}
-		if (income[i] < min) {
-			min = income[i];
-		}
+		cout << "Enter range (firstMonth lastMonth): ";
+		cin >> firstMonth >> lastMonth;
+		firstMonth -= 1;
+		lastMonth -= 1;
+	} while (!(firstMonth >= 0 && firstMonth < SIZE && lastMonth >= 0 && lastMonth < SIZE));
+
+	if (firstMonth > lastMonth) {
+		int temp = firstMonth;
+		firstMonth = lastMonth;
+		lastMonth = temp;
 	}
-	cout << "Max monthly income: " << max << "; Min monthly income " << min << endl;
+
+	int sum = 0;
+	for (size_t i = firstMonth; i <= lastMonth; i++)
+	{
+		sum += income[i];
+	}
+	cout << "Total sum of range from " << firstMonth + 1 << " to " << lastMonth + 1 << " is equal to " << sum << endl;
 	return 0;
 }
