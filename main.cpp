a8d83d1880260964397b7289f5a6278166eac9d4
Bashmeg <superbashmeg@gmail.com>
2024-05-15 20:07:08 +0300

    hw task 4 done

--- main.cpp
+++ main.cpp
@@ -1,34 +1,104 @@
 #include <iostream>
+#include <conio.h>
+#include <iomanip>
 using namespace std;
 
-int main() {
-	const int SIZE = 12;
-	int firstMonth, lastMonth;
-	int income[SIZE];
-	for (size_t i = 0; i < SIZE; i++)
+double findMaxElementOfArray(double arr[], int size) {
+	int maxI = 0;
+	double max = arr[0];
+	for (size_t i = 0; i < size; i++)
 	{
-		cout << "Enter income for " << i + 1 << " month: ";
-		cin >> income[i];
+		if (arr[i] > max) {
+			maxI = i;
+			max = arr[i];
+		}
 	}
+	return maxI;
+}
+
+double findMinElementOfArray(double arr[], int size) {
+	int minI = 0;
+	double min = arr[0];
+	for (size_t i = 0; i < size; i++)
+	{
+		if (arr[i] < min) {
+			minI = i;
+			min = arr[i];
+		}
+	}
+	return minI;
+}
+
+void printTask1(double arr[], int size) {
+	double sum = 0;
+	for (size_t i = 0; i < size; i++)
+	{
+		if (arr[i] < 0) {
+			sum += arr[i];
+		}
+	}
+	cout << "Sum of all negative elements is - " << setprecision(20) << sum << endl;
+}
+
+void printTask2(double arr[], int size) {
+	double min = findMinElementOfArray(arr, size);
+	double max = findMaxElementOfArray(arr, size);
+	double sum = 1;
+	for (size_t i = min; i < max-1; i++)
+	{
+		sum *= arr[i];
+	}
+	cout << "Multiplication of all elements between " << min+1 << " and " << max+1 << " is - " << sum << endl;
+}
+
+void printTask3(double arr[], int size) {
+	double sum = 1;
+	for (size_t i = 0; i < size; i+=2)
+	{
+		sum *= arr[i];
+	}
+	cout << "Multiplication of all elements with even position - " << setprecision(20) << sum << endl;
+}
+
+void printTask4(double arr[], int size) {
+	int firstNegativeI = 0;
+	int lastNegativeI = 0;
+	double sum = 1;
+	for (size_t i = 0; i < size; i++)
+	{
+		if (arr[i] < 0 && !firstNegativeI) {
+			firstNegativeI = i;
+		}
+		if (arr[i] < 0) {
+			lastNegativeI = i;
+		}
+	}
+	for (size_t i = firstNegativeI + 1; i < lastNegativeI; i++)
+	{
+		sum *= arr[i];
+	}
+	cout << "Sum of all elements between first and last negatives - " << setprecision(20) << sum << endl;
+}
+
+int main() {
+	const int maxMemory = 1000;
+	int realMemory = 0;
 	do
 	{
-		cout << "Enter range (firstMonth lastMonth): ";
-		cin >> firstMonth >> lastMonth;
-		firstMonth -= 1;
-		lastMonth -= 1;
-	} while (!(firstMonth >= 0 && firstMonth < SIZE && lastMonth >= 0 && lastMonth < SIZE));
-
-	if (firstMonth > lastMonth) {
-		int temp = firstMonth;
-		firstMonth = lastMonth;
-		lastMonth = temp;
-	}
-
-	int sum = 0;
-	for (size_t i = firstMonth; i <= lastMonth; i++)
+		cout << "Enter Size Of Array: ";
+		cin >> realMemory;
+	} while (!(realMemory > 0 && realMemory < maxMemory));
+	
+	int firstMonth, lastMonth;
+	double numbers[maxMemory];
+	for (size_t i = 0; i < realMemory; i++)
 	{
-		sum += income[i];
+		cout << "Enter number for " << i + 1 << " element of an array: ";
+		cin >> numbers[i];
 	}
-	cout << "Total sum of range from " << firstMonth + 1 << " to " << lastMonth + 1 << " is equal to " << sum << endl;
+	printTask1(numbers, realMemory);
+	printTask2(numbers, realMemory);
+	printTask3(numbers, realMemory);
+	printTask4(numbers, realMemory);
 	return 0;
 }
