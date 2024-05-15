23b8c4d0ea5df18a6cce378a0d92359fecbe669a
Bashmeg <superbashmeg@gmail.com>
2024-05-15 18:49:18 +0300

    lab task 1 done

--- main.cpp
+++ main.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main() {
-
+	int sum = 0;
+	const int SIZE = 6;
+	int income[SIZE];
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		cout << "Enter income for " << i + 1 << " month: ";
+		cin >> income[i];
+	}
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		sum += income[i];
+	}
+	cout << "Total income: " << sum << endl;
 	return 0;
 }
