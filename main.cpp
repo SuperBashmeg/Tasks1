3d1df468b935268b3b09bc367f62e176f5bf8d98
Bashmeg <superbashmeg@gmail.com>
2024-05-15 18:58:31 +0300

    lab task 4 done

--- main.cpp
+++ main.cpp
@@ -2,18 +2,23 @@
 using namespace std;
 
 int main() {
-	int sum = 0;
-	const int SIZE = 6;
+	const int SIZE = 12;
 	int income[SIZE];
 	for (size_t i = 0; i < SIZE; i++)
 	{
 		cout << "Enter income for " << i + 1 << " month: ";
 		cin >> income[i];
 	}
+	int max = income[0], min = income[0];
 	for (size_t i = 0; i < SIZE; i++)
 	{
-		sum += income[i];
+		if (income[i] > max) {
+			max = income[i];
+		}
+		if (income[i] < min) {
+			min = income[i];
+		}
 	}
-	cout << "Total income: " << sum << endl;
+	cout << "Max monthly income: " << max << "; Min monthly income " << min << endl;
 	return 0;
 }
