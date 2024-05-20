30e4fda9ac7f9aa73a89ccd6fb7db937300db2c8
Bashmeg <superbashmeg@gmail.com>
2024-05-20 20:06:15 +0300

    lesson 6 task 2 done

--- main.cpp
+++ main.cpp
@@ -2,31 +2,41 @@
 using namespace std;
 int main()
 {
-    const int size = 9;
-    double arr[size] = { -5.7, 6.0, 2, 0, -4.7, 6, 8.1, -4, 0 };
+	const int SIZE = 40;
+	int arr[SIZE];
 
-    double positive = 0;
-    for (int i = size - 1; i >= 0; i--)
-    {
-        if (arr[i] > 0)
-        {
-            positive = arr[i];
-            break;
-        }
-    }
+	for (int i = 0; i < SIZE; i++) {
+		cout << "Enter the population of the " << i + 1 << " flat: ";
+		cin >> arr[i];
+	}
+	int flat;
+	cout << "Enter the number of the flat: ";
+	cin >> flat;
+	int population = arr[flat - 1];
+	if (flat % 2 == 1) {
+		population += arr[flat];
+	}
+	else
+	{
+		population += arr[flat - 2];
+	}
+	cout << "The population of the same floor is " << population << endl;
 
-    double negative = 0;
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] < 0)
-        {
-            negative = arr[i];
-            break;
-        }
-    }
+	int enterence;
+	cout << "Enter the number of the enterence: ";
+	cin >> enterence;
+	int enterencePopulation = 0;
+	for (int i = (enterence - 1) * 10; i < (enterence - 1) * 10 + 10; i++) {
+		enterencePopulation += arr[i];
+	}
+	cout << "The population of the same enterence is " << enterencePopulation << endl;
 
-    cout << "Last positive number: " << positive << endl;
-    cout << "First negative number: " << negative << endl;
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		if (arr[i] >= 5) {
+			cout << "The number of the flat with more than 5 people is " << i + 1 << endl;
+		}
+	}
 
-    return 0;
+	return 0;
 }
