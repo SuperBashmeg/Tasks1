88f2dbd3655ed100085ea2b8233026ec9bd15c79
Bashmeg <superbashmeg@gmail.com>
2024-05-20 19:12:21 +0300

    lab task 2 done

--- main.cpp
+++ main.cpp
@@ -5,41 +5,67 @@ int main() {
 	const int SIZE = 5;
 	int currentSize = SIZE;
 	int arr[SIZE] = { 1, 0, 2, 3, 4 };
+	int arr2[SIZE] = { -3, 8, -1, 2, 0 };
+	int globalArray[SIZE * 2];
+	int globalCurrentSize = 0;
 
 	int index;
 	for (size_t i = 0; i < SIZE; i++)
 	{
-		if (arr[i] == 0)
+		if (arr[i] > 0)
 		{
-			index = i;
-			currentSize--;
-			cout << currentSize << endl;
+			globalArray[globalCurrentSize] = arr[i];
+			globalCurrentSize++;
 		}
-		else
+	}
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		if (arr2[i] > 0)
 		{
-			index = -1;
-		}
-		for (size_t j = 0; j <= currentSize; j++)
-		{
-			if (j >= index && index != -1)
-			{
-				arr[j] = arr[j + 1];
-				if (j == currentSize)
-				{
-					arr[j] = -1;
-					i = i - 1;
-				}
-			}
-			else
-			{
-				arr[j] = arr[j];
-			}
+			globalArray[globalCurrentSize] = arr2[i];
+			globalCurrentSize++;
 		}
 	}
 
 	for (size_t i = 0; i < SIZE; i++)
 	{
-		cout << arr[i] << " ";
+		if (arr[i] == 0)
+		{
+			globalArray[globalCurrentSize] = arr[i];
+			globalCurrentSize++;
+		}
+	}
+
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		if (arr2[i] == 0)
+		{
+			globalArray[globalCurrentSize] = arr2[i];
+			globalCurrentSize++;
+		}
+	}
+
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		if (arr[i] < 0)
+		{
+			globalArray[globalCurrentSize] = arr[i];
+			globalCurrentSize++;
+		}
+	}
+
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		if (arr2[i] < 0)
+		{
+			globalArray[globalCurrentSize] = arr2[i];
+			globalCurrentSize++;
+		}
+	}
+
+	for (size_t i = 0; i < SIZE*2; i++)
+	{
+		cout << globalArray[i] << " ";
 	}
 	
 	return 0;
