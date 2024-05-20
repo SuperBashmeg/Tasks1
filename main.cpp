62f8b2e63b495bf07200c6a87801436c0e863f37
Bashmeg <superbashmeg@gmail.com>
2024-05-20 20:14:35 +0300

    lesson 6 task 4 done

--- main.cpp
+++ main.cpp
@@ -2,33 +2,34 @@
 using namespace std;
 int main()
 {
-	const int SIZE = 31;
-	double temp[SIZE];
+	const int SIZE = 10;
+	double cars[SIZE];
 
 	for (int i = 0; i < SIZE; i++)
 	{
-		cout << "Enter the temperature for day " << i + 1 << ": ";
-		cin >> temp[i];
+		cout << "Enter the price of car " << i + 1 << ": ";
+		cin >> cars[i];
 	}
 
-	double sum = 0;
-	for (int i = 0; i < SIZE; i++)
+	double max = cars[0];
+	int maxIndex = 0;
+	int maxIndex2 = -1;
+	for (int i = 1; i < SIZE; i++)
 	{
-		sum += temp[i];
-	}
-	cout << "The avarage temperature for jenuary is: " << sum / SIZE << endl;
-
-	double mark;
-	cout << "Enter the temperature mark: ";
-	cin >> mark;
-
-	cout << "The days with temperature less than " << mark << " are: ";
-	for (int i = 0; i < SIZE; i++)
-	{
-		if (temp[i] < mark)
+		if (cars[i] > max)
 		{
-			cout << i + 1 << " ";
+			max = cars[i];
+			maxIndex = i;
 		}
+		else if (cars[i] == max)
+		{
+			maxIndex2 = i;
+		}
+	}
+	cout << "The most expensive car is car " << maxIndex + 1 << " with the price of " << max << endl;
+	if (maxIndex2 != -1)
+	{
+		cout << "The last most expensive car is car " << maxIndex2 + 1 << " with the price of " << max << endl;
 	}
 
 
