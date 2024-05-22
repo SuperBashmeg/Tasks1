161b2b79a759f49ffb2abc21d40be8fa82a340da
Bashmeg <superbashmeg@gmail.com>
2024-05-22 19:36:43 +0300

    lab task 3 started

--- main.cpp
+++ main.cpp
@@ -17,60 +17,28 @@ void Align(int number) {
 int main() {
 	srand(time(0));
 	const int rows = 5;
-	const int cols = 2;
+	const int cols = 10;
 	int matrix[rows][cols];
-	int rowsSum[rows];
-	int colsSum[cols];
-	int globalSum = 0;
+	int matrix2[rows][cols/2];
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
-			matrix[i][j] = rand() % 25;
-		}
-	}
-
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < cols; j++) {
-			cout << matrix[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-	for (int i = 0; i < rows; i++) {
-		rowsSum[i] = 0;
-		for (int j = 0; j < cols; j++) {
-			rowsSum[i] += matrix[i][j];
-			globalSum += matrix[i][j];
-		}
-	}
-
-	for (int i = 0; i < cols; i++) {
-		colsSum[i] = 0;
-		for (int j = 0; j < rows; j++) {
-			colsSum[i] += matrix[j][i];
-			globalSum += matrix[j][i];
+			matrix[i][j] = rand() % 50;
 		}
 	}
 
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
 			Align(matrix[i][j]);
-			cout << matrix[i][j];
+			cout << matrix[i][j] << " ";
 		}
-		cout << "   |";
-		Align(rowsSum[i]);
-		cout << rowsSum[i] << endl;
+		cout << endl;
 	}
-	for (int i = 0; i < 2+cols; i++) {
+	for (int i = 0; i < cols+2; i++) {
 		cout << "----";
 	}
+	cout << "-";
 	cout << endl;
-	for (int i = 0; i < cols; i++) {
-		Align(colsSum[i]);
-		cout << colsSum[i];
-	}
-	cout << "   |";
-	Align(globalSum);
-	cout << globalSum << endl;
+
 
 	return 0;
 }
