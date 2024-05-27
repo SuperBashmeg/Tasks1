0c58da5e833c61ff793e840c09f4321c31d1abab
Bashmeg <superbashmeg@gmail.com>
2024-05-27 19:07:42 +0300

    hw task 3 done

--- main.cpp
+++ main.cpp
@@ -1,20 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int GetSumOfNumbersBetween(int start, int end) {
-	int sum = 0;
-	for (int i = start+1; i < end; i++) {
-		sum += i;
+void GetPerfectNumbers(int start, int end) {
+	bool isPerfect = false;
+	for (int i = start; i <= end; i++) {
+		int sumOfDivisors = 0;
+		for (int j = 1; j < i; j++) {
+			if (i % j == 0) {
+				sumOfDivisors += j;
+			}
+		}
+		if (sumOfDivisors == i) {
+			if (!isPerfect) {
+				cout << "Perfect Numbers: ";
+			}
+			else
+			{
+				cout << "; ";
+			}
+			cout << i;
+			isPerfect = true;
+		}
 	}
-	return sum;
+	if (!isPerfect) {
+		cout << "There are no perfect numbers in the range.";
+	}
+	else
+	{
+		cout << endl;
+	}
 }
 
 int main() {
 	int start, end;
-	cout << "Enter start: ";
+	cout << "Enter the start of the range: ";
 	cin >> start;
-	cout << "Enter end: ";
+	cout << "Enter the end of the range: ";
 	cin >> end;
-	cout << "Sum of numbers between " << start << " and " << end << " is " << GetSumOfNumbersBetween(start, end) << endl;
+	GetPerfectNumbers(start, end);
 	return 0;
 }
