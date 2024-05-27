285671736f5c6d8e7b50bdcaf5ffe6ac424194f4
Bashmeg <superbashmeg@gmail.com>
2024-05-27 18:41:17 +0300

    lab task 1 done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void printSqauare(int w, int h) {
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			cout << "* ";
+		}
+		cout << endl;
+	}
+}
 
+int main() {
+	printSqauare(5, 9);
 	return 0;
 }
