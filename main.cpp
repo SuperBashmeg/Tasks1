4350ccf688313602d78e29bf19a17adf527b9dad
Bashmeg <superbashmeg@gmail.com>
2024-05-27 18:44:04 +0300

    lab task 2 done

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void printSqauare(int w, int h) {
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < w; j++) {
-			cout << "* ";
-		}
-		cout << endl;
+long long factorial(int w) {
+	long long fact = 1;
+	for (int i = 1; i <= w; i++) {
+		fact *= i;
 	}
+	return fact;
 }
 
 int main() {
-	printSqauare(5, 9);
+	//Enter the number for which you want to find the factorial
+	int num;
+	cout << "Enter the number for which you want to find the factorial: ";
+	cin >> num;
+	long long fact = factorial(num);
+	cout << "Factorial of " << num << " is " << fact << endl;
 	return 0;
 }
