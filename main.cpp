6cebd62c44e503bf867cc6da7a3535fb296e8b6b
Bashmeg <superbashmeg@gmail.com>
2024-05-27 18:48:09 +0300

    lab task 4 done

--- main.cpp
+++ main.cpp
@@ -1,24 +1,14 @@
 #include <iostream>
 using namespace std;
 
-bool isNumberPrime(int num) {
-	for (int i = 2; i < num; i++) {
-		if (num % i == 0) {
-			return false;
-		}
-	}
-	return true;
+int GetNumberCube(int num) {
+	return num * num * num;
 }
 
 int main() {
 	int num;
 	cout << "Enter a number: ";
 	cin >> num;
-	if (isNumberPrime(num)) {
-		cout << num << " is a prime number." << endl;
-	}
-	else {
-		cout << num << " is not a prime number." << endl;
-	}
+	cout << "The cube of " << num << " is " << GetNumberCube(num) << endl;
 	return 0;
 }
