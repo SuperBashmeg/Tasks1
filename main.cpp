339c023891d7d3c34926965896fe0b4643631b27
Bashmeg <superbashmeg@gmail.com>
2024-05-27 20:37:16 +0300

    hw task 5 done

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int GetNumber(int base, int power) {
-	return pow(base, power);
+bool IsNumberLucky(int number) {
+	int sum1 = 0, sum2 = 0;
+	for (int i = 0; i < 3; i++) {
+		sum1 += number % 10;
+		number /= 10;
+	}
+	for (int i = 0; i < 3; i++) {
+		sum2 += number % 10;
+		number /= 10;
+	}
+	return sum1 == sum2;
 }
 
 int main() {
-	int base, power;
-	cout << "Enter the base: ";
-	cin >> base;
-	cout << "Enter the power: ";
-	cin >> power;
-	cout << "The result is: " << GetNumber(base, power) << endl;
-	return 0;
+	int number;
+	do
+	{
+		cout << "Enter a 6 digit number: ";
+		cin >> number;
+	} while (!(number >= 99999 && number < 1000000));
+
+	if (IsNumberLucky(number))
+		cout << "The number is lucky." << endl;
+	else
+		cout << "The number is not lucky." << endl;
 }
