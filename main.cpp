2dd3f195b12aa2456d6afab87c75a678c96c9c19
Bashmeg <superbashmeg@gmail.com>
2024-06-03 19:51:34 +0300

    lab task 4 done

--- main.cpp
+++ main.cpp
@@ -1,22 +1,34 @@
 #include "lib.h"
 
-int sonbanb(int a, int b, int currentsum) { //Sum of numbers between a and b
-	if (a == b) {
-		return currentsum+a;
+int gsotce(int arr[], int size, int startIndex, int endIndex, int currentIndex, int totalsum) { //get sum of ten consecutive elements
+	if (currentIndex == endIndex) {
+		return totalsum;
 	}
-	else {
-		currentsum += a;
-		sonbanb(a + 1, b, currentsum);
+	totalsum += arr[currentIndex];
+	gsotce(arr, size, startIndex, endIndex, currentIndex + 1, totalsum);
+}
+
+int gpomctn(int arr[], int size, int minimalindex, int lastindex) { //get position of minimal consecutive ten numbers
+	if (lastindex == size - 11) {
+		return minimalindex;
 	}
+	if (gsotce(arr, size, lastindex, lastindex + 10, lastindex, 0) < gsotce(arr, size, minimalindex, minimalindex + 10, minimalindex, 0)) {
+		minimalindex = lastindex;
+	}
+	gpomctn(arr, size, minimalindex, lastindex + 1);
 }
 
 int main() {
-	//enter a and b
-	int a, b;
-	cout << "Enter a: ";
-	cin >> a;
-	cout << "Enter b: ";
-	cin >> b;
-	cout << "Sum of numbers between a and b: " << sonbanb(a, b, 0) << endl;
+	srand(time(0));
+	const int SIZE = 100;
+	int arr[SIZE];
+	for (int i = 0; i < SIZE; i++) {
+		arr[i] = rand() % 250;
+	}
+	for (int i = 0; i < SIZE; i++) {
+		cout << arr[i] << " ";
+	}
+	gpomctn(arr, SIZE, 0, 0);
+	cout << endl << "Position of minimal consecutive ten numbers: " << gpomctn(arr, SIZE, 0, 0) << endl;
 	return 0;
 }
