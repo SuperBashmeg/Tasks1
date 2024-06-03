c6acd632f03589861d863746a39ffee80896e0a7
Bashmeg <superbashmeg@gmail.com>
2024-06-03 19:31:47 +0300

    lab task 3 done

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include "lib.h"
 
-void aos(int n) { //amount of stars
-	if (n == 0) {
-		return;
+int sonbanb(int a, int b) { //Sum of numbers between a and b
+	if (a == b) {
+		return a;
 	}
 	else {
-		cout << "*";
-		aos(n - 1);
+		return a + sonbanb(a + 1, b);
 	}
 }
 
 int main() {
-	int n;
-	cout << "Enter amount of stars: ";
-	cin >> n;
-	aos(n);
+	//enter a and b
+	int a, b;
+	cout << "Enter a: ";
+	cin >> a;
+	cout << "Enter b: ";
+	cin >> b;
+	cout << "Sum of numbers between a and b: " << sonbanb(a, b) << endl;
 	return 0;
 }
