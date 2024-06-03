e3b3692abfe95ded1b0c0cfa347d91773202ff3b
Bashmeg <superbashmeg@gmail.com>
2024-06-03 20:37:08 +0300

    hw task 2 done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include "lib.h"
 
-int main() {
+int gcd(int a, int b) { //recursive greatest common divisor
+	if (b == 0) return a;
+	return gcd(b, a % b);
+}
 
+int main() {
+	int a, b;
+	cout << "Enter two numbers: ";
+	cin >> a >> b;
+	cout << "Greates Common Divisor of " << a << " and " << b << " is " << gcd(a, b) << endl;
 	return 0;
 }
