037604cab4750bd5266f2933bcb7bf015a019ce7
Bashmeg <superbashmeg@gmail.com>
2024-06-03 19:26:00 +0300

    lab task 1 done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include "lib.h"
 
-int main() {
+int pon(int number, int power, int result) { // Power of number
+	if (power == 0) {
+		return result;
+	}
+	else {
+		result = result * number;
+		return pon(number, power - 1, result);
+	}
+}
 
+int main() {
+	int number;
+	cout << "Enter number: ";
+	cin >> number;
+	int power;
+	cout << "Enter power: ";
+	cin >> power;
+	int answer = pon(number, power, 1);
+	cout << "Answer: " << answer << endl;
 	return 0;
 }
