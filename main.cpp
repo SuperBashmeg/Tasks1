1dfa7d038fd43361b1e9aedc24a8b89daba94c7f
Bashmeg <superbashmeg@gmail.com>
2024-06-10 18:46:21 +0300

    lab taask 1 doneee

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include "lib.h"
 
+template<typename T>T* 
+init_arr(int size) 
+{
+	T* arr = new T[size];
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 100;
+	}   
+	return arr;
+}
+
+template<typename T>
+void print_arr(T* arr, int size)
+{
+	for (int i = 0; i < size; i++) {
+		cout << *(arr + i) << " ";
+	}
+	cout << endl; }
+
 int main() {
+	srand(time(0));
+	int arr_size_1;
+	int arr_size_2;
+	cout << "Enter the size of the first array: ";
+	cin >> arr_size_1;
+	cout << "Enter the size of the second array: ";
+	cin >> arr_size_2;
+	int* arr3 = init_arr<int>(arr_size_1+arr_size_2);
+	cout << "Array:";
+	print_arr(arr3, arr_size_1 + arr_size_2);
 
 	return 0;
 }
