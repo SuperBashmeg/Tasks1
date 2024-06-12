33e6fae63981148bafe6004abb87eb08e1a18514
Bashmeg <superbashmeg@gmail.com>
2024-06-12 18:59:27 +0300

    lab task 1 done

--- main.cpp
+++ main.cpp
@@ -19,83 +19,37 @@ void print_arr(T* arr, int size)
 	}
 	cout << endl;
 }
-template<typename T>
-void add_elem_to_arr(T*& arr, int& size, T elem, int index) {
-	size++;
-	T* new_arr = new T[size];
-	for (int i = 0; i < size; i++) {
-		if (i < index) {
-			new_arr[i] = arr[i];
-		}
-		else if (i == index) {
-			new_arr[i] = elem;
-		}
-		else {
-			new_arr[i] = arr[i - 1];
-		}
-	}
-	delete[] arr;
-	arr = new_arr; 
-}
-
-int find_elem_in_arr(int* arr, int size, int elem) {
-	for (int i = 0; i < size; i++) {
-		if (arr[i] == elem) {
-			return i;
-		}
-	}
-	return -1;
-}
 
 template<typename T>
-void create_array_from_2_arrays(T* arr1, T* arr2, int size1, int size2) {
-	int new_array_size = 0;
-	bool is_number_found = false;
-	T* new_arr = new T[0];
-	for (int i = 0; i < size1; i++) {
-		for (int j = 0; j < size2; j++) {
-			if (arr1[i] == arr2[j]) {
-				is_number_found = true;
-			}
-		}
-		if (!is_number_found && find_elem_in_arr(new_arr, new_array_size, arr1[i]) == -1) {
-			add_elem_to_arr<int>(new_arr, new_array_size, arr1[i], new_array_size);
-		}
-		is_number_found = false;
+void func(T* arr, int size, T*& sum, T*& mul) {
+	if (sum == nullptr) {
+		sum = new T(0);
 	}
-	for (int i = 0; i < size2; i++) {
-		for (int j = 0; j < size1; j++) {
-			if (arr2[i] == arr1[j]) {
-				is_number_found = true;
-			}
-		}
-		if (!is_number_found && find_elem_in_arr(new_arr, new_array_size, arr2[i]) == -1) {
-			add_elem_to_arr<int>(new_arr, new_array_size, arr2[i], new_array_size);
-		}
-		is_number_found = false;
+	if (mul == nullptr) {
+		mul = new T(1);
 	}
-
-	print_arr<int>(new_arr, new_array_size);
+	*sum = 0;
+	*mul = 1;
+	for (int i = 0; i < size; i++) {
+		*sum += *(arr + i);
+		*mul *= *(arr + i);
+	}
 }
-
-
-
 
 
 int main() {
 	srand(time(0));
-	int arr_size_1;
-	int arr_size_2;
+	int arr_size;
 	cout << "Enter the size of the first array: ";
-	cin >> arr_size_1;
-	cout << "Enter the size of the second array: ";
-	cin >> arr_size_2;
-	int* arr1 = init_arr<int>(arr_size_1);
-	int* arr2 = init_arr<int>(arr_size_2);
-	int newsize = 0;
-	print_arr<int>(arr1, arr_size_1);
-	print_arr<int>(arr2, arr_size_2);
-	create_array_from_2_arrays<int>(arr1, arr2, arr_size_1, arr_size_2);
+	cin >> arr_size;
+	int* arr = init_arr<int>(arr_size);
+	print_arr<int>(arr, arr_size);
+	int* sum = nullptr;
+	int* mul = nullptr;
+	func<int>(arr, arr_size, sum, mul);
+	cout << "Sum: " << *sum << endl;
+	cout << "Mul: " << *mul << endl;
+
 
 
 	return 0;
