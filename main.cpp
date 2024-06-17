984ba059d8e04bf1681838a89ea236dc7353317f
Bashmeg <superbashmeg@gmail.com>
2024-06-17 19:03:11 +0300

    hw task 3 done (ez)

--- main.cpp
+++ main.cpp
@@ -31,6 +31,72 @@ void randMatrix(T** arr, int row, int col)
         }
     }
 }
+
+template <typename T>
+void shiftMatrix(T** arr, int row, int col, int times, string direction)
+{
+    if (direction == "left")
+    {
+        for (int t = 0; t < times; t++)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                T temp = arr[i][0];
+                for (int j = 0; j < col - 1; j++)
+                {
+                    arr[i][j] = arr[i][j + 1];
+                }
+                arr[i][col - 1] = temp;
+            }
+        }
+    }
+    else if (direction == "right")
+    {
+        for (int t = 0; t < times; t++)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                T temp = arr[i][col - 1];
+                for (int j = col - 1; j > 0; j--)
+                {
+                    arr[i][j] = arr[i][j - 1];
+                }
+                arr[i][0] = temp;
+            }
+        }
+    }
+    else if (direction == "up")
+    {
+        for (int t = 0; t < times; t++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                T temp = arr[0][j];
+                for (int i = 0; i < row - 1; i++)
+                {
+                    arr[i][j] = arr[i + 1][j];
+                }
+                arr[row - 1][j] = temp;
+            }
+        }
+    }
+    else if (direction == "down")
+    {
+        for (int t = 0; t < times; t++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                T temp = arr[row - 1][j];
+                for (int i = row - 1; i > 0; i--)
+                {
+                    arr[i][j] = arr[i - 1][j];
+                }
+                arr[0][j] = temp;
+            }
+        }
+    }
+}
+
 template <typename T>
 void printMatrix(T** arr, int row, int col)
 {
@@ -44,20 +110,6 @@ void printMatrix(T** arr, int row, int col)
     }
 }
 
-template <typename T>
-void removeColumn(T** arr, int row, int& col, int index)
-{
-	for (int i = 0; i < row; i++)
-	{
-		for (int j = index; j < col - 1; j++)
-		{
-			arr[i][j] = arr[i][j + 1];
-		}
-	}
-	col--;
-}
-
-
 int main()
 {
     srand(time(0));
@@ -66,8 +118,9 @@ int main()
     int** arr = createMatrix<int>(row, col);
     randMatrix(arr, row, col);
     printMatrix(arr, row, col);
-	cout << endl;
-	removeColumn(arr, row, col, 2);
-	printMatrix(arr, row, col);
+    cout << endl;
+    shiftMatrix(arr, row, col, 1, "right");
+    printMatrix(arr, row, col);
     deleteMatrix(arr, row);
+
 }
