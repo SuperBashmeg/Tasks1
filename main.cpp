de4899eb1221c709bbd00cd7e68b720cbd0f9df7
Bashmeg <superbashmeg@gmail.com>
2024-06-19 19:25:50 +0300

    hw task 4 done

--- main.cpp
+++ main.cpp
@@ -1,27 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "lib.h"
 
-void insertChar(char* str, char charToAdd, int indexToInsert) {
-	int i = strlen(str);
-	while (i >= indexToInsert) {
-		str[i + 1] = str[i];
-		i--;
+void replace(char* str) {
+	for (int i = 0; str[i] != '\0'; i++) {
+		if (str[i] == '.') {
+			str[i] = '!';
+		}
 	}
-	str[indexToInsert] = charToAdd;
 }
 
 int main() {
 	const int SIZE = 100;
-	char str[SIZE] = "Just some random string";
-	char charToAdd;
-	int indexToInsert;
-	cout << "Original string: " << str << endl;
-	cout << "Enter the character to add: ";
-	cin >> charToAdd;
-	cout << "Enter the index to insert the character: ";
-	cin >> indexToInsert;
-	insertChar(str, charToAdd, indexToInsert);
-	cout << "String after inserting the character: " << str << endl;
+	char str[SIZE];
+	cout << "Enter the string: ";
+	cin.getline(str, SIZE);
+	replace(str);
+	cout << "String after replacment the characters: " << str << endl;
 	
 	return 0;
 }
