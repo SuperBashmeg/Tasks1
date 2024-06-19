af87c241df6002409beac47bd63623b05fde38f8
Bashmeg <superbashmeg@gmail.com>
2024-06-19 18:51:58 +0300

    lab task 1 done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include "lib.h"
 
 int main() {
-
+	const int SIZE = 100;
+	char str[SIZE] = "Hello, World !";
+	int i = 0;
+	while (str[i] != '\0') {
+		if (str[i] == ' ') {
+			str[i] = '\t';
+		}
+		i++;
+	}
+	cout << str << endl;
 	return 0;
 }
