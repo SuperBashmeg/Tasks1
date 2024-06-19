ea7912636fceee9fd58950fdf0ad1895f0231a10
Bashmeg <superbashmeg@gmail.com>
2024-06-19 18:54:48 +0300

    lab task 2 done

--- main.cpp
+++ main.cpp
@@ -1,15 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "lib.h"
 
 int main() {
 	const int SIZE = 100;
-	char str[SIZE] = "Hello, World !";
+	char str[SIZE] = "Hello, World!123";
 	int i = 0;
+	int digits = 0;
+	int letters = 0;
+	int other = 0;
+
 	while (str[i] != '\0') {
-		if (str[i] == ' ') {
-			str[i] = '\t';
+		if (isdigit(str[i])) {
+			digits++;
+		}
+		else if (isalpha(str[i])) {
+			letters++;
+		}
+		else {
+			other++;
 		}
 		i++;
 	}
-	cout << str << endl;
+	cout << "Digits: " << digits << endl;
+	cout << "Letters: " << letters << endl;
+	cout << "Other: " << other << endl;
 	return 0;
 }
