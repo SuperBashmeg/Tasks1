f85ca103ee1ca856e80d461206d6e27605f4bc52
Bashmeg <superbashmeg@gmail.com>
2024-06-19 19:23:52 +0300

    hw task 3 done

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "lib.h"
 
-void deleteChar(char* str, char charToDelete) {
-	int i = 0, j = 0;
-	while (str[i]) {
-		if (str[i] != charToDelete) {
-			str[j] = str[i];
-			j++;
-		}
-		i++;
+void insertChar(char* str, char charToAdd, int indexToInsert) {
+	int i = strlen(str);
+	while (i >= indexToInsert) {
+		str[i + 1] = str[i];
+		i--;
 	}
-	str[j] = '\0';
+	str[indexToInsert] = charToAdd;
 }
 
 int main() {
 	const int SIZE = 100;
 	char str[SIZE] = "Just some random string";
-	char charToDelete;
+	char charToAdd;
+	int indexToInsert;
 	cout << "Original string: " << str << endl;
-	cout << "Enter the character you want to delete: ";
-	cin >> charToDelete;
-	deleteChar(str, charToDelete);
-	cout << "String after deleting the character: " << str << endl;
+	cout << "Enter the character to add: ";
+	cin >> charToAdd;
+	cout << "Enter the index to insert the character: ";
+	cin >> indexToInsert;
+	insertChar(str, charToAdd, indexToInsert);
+	cout << "String after inserting the character: " << str << endl;
+	
 	return 0;
 }
