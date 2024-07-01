051e1856dd862aa9cc5f09d1641dee2836876189
Bashmeg <superbashmeg@gmail.com>
2024-07-01 19:00:37 +0300

    task 2 done

--- main.cpp
+++ main.cpp
@@ -1,33 +1,77 @@
 #include "lib.h"
 
-//guess the number game
-int main() {
-	int startTimer = time(0);
-	int number = 0;
-	int guess = 0;
-	int tries = 0;
-	srand(time(0));
-	number = rand() % 500 + 1;
-	cout << "Guess My Number Game\n\n";
-	do {
-		cout << "Enter a guess between 1 and 500 : ";
-		cin >> guess;
-		tries++;
-		if (guess == 0) {
-			cout << "You have entered 0. Exiting the game. Number was - " << number;
-			break;
-		}
-		else if (guess > number) {
-			cout << "Too high! Enter lower number!\n\n";
-		}
-		else if (guess < number) {
-			cout << "Too low! Enter higher number!\n\n";
-		}
-		else {
-			cout << "\nCorrect! You got it in " << tries << " guesses!\n";
-			cout << "Time taken to guess the number: " << time(0) - startTimer << " seconds\n";
-		}
-	} while (guess != number);
+double eurToUah(double amount) {
+	return amount * 33.5;
+}
 
+double uahToEur(double amount) {
+	return amount / 33.5;
+}
+
+double usdToUah(double amount) {
+	return amount * 28.1;
+}
+
+double uahToUsd(double amount) {
+	return amount / 28.1;
+}
+
+double eurToUsd(double amount) {
+	return amount * 1.18;
+}
+
+double usdToEur(double amount) {
+	return amount / 1.18;
+}
+
+int main() {
+	cout << "Currency Converter" << endl;
+	cout << "1. EUR to UAH" << endl;
+	cout << "2. UAH to EUR" << endl;
+	cout << "3. USD to UAH" << endl;
+	cout << "4. UAH to USD" << endl;
+	cout << "5. EUR to USD" << endl;
+	cout << "6. USD to EUR" << endl;
+	cout << "7. Exit" << endl;
+	cout << "Enter your choice: ";
+	int choice;
+	cin >> choice;
+	double amount;
+	switch (choice) {
+	case 1:
+		cout << "Enter amount of EUR: ";
+		cin >> amount;
+		cout << "Amount of UAH: " << eurToUah(amount) << endl;
+		break;
+	case 2:
+		cout << "Enter amount of UAH: ";
+		cin >> amount;
+		cout << "Amount of EUR: " << uahToEur(amount) << endl;
+		break;
+	case 3:
+		cout << "Enter amount of USD: ";
+		cin >> amount;
+		cout << "Amount of UAH: " << usdToUah(amount) << endl;
+		break;
+	case 4:
+		cout << "Enter amount of UAH: ";
+		cin >> amount;
+		cout << "Amount of USD: " << uahToUsd(amount) << endl;
+		break;
+	case 5:
+		cout << "Enter amount of EUR: ";
+		cin >> amount;
+		cout << "Amount of USD: " << eurToUsd(amount) << endl;
+		break;
+	case 6:
+		cout << "Enter amount of USD: ";
+		cin >> amount;
+		cout << "Amount of EUR: " << usdToEur(amount) << endl;
+		break;
+	case 7:
+		return 0;
+	default:
+		cout << "Invalid choice" << endl;
+	}
 	return 0;
 }
