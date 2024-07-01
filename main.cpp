ddb8cd52428ef647457afafad9a6e3f83e091ba7
Bashmeg <superbashmeg@gmail.com>
2024-07-01 18:44:14 +0300

    task 3 done

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main() {
-	int x;
-	cout << "Enter a number: ";
-	cin >> x;
-	int y;
-	cout << "Enter another number: ";
-	cin >> y;
-	cout << pow(x, y) << endl;
+	int sum = 0;
+	double average = 0;
+	for (int i = 1; i <= 1000; i++) {
+		sum += i;
+	}
+	average = sum / 1000.0;
+	cout << "Sum: " << sum << endl;
+	cout << "Average: " << average << endl;
 }
