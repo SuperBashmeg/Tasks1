3147702b74654237073f4914768fb4df26daf444
Bashmeg <superbashmeg@gmail.com>
2024-07-01 19:06:49 +0300

    task 2 done

--- main.cpp
+++ main.cpp
@@ -1,20 +1,14 @@
 #include "lib.h"
 
-bool hasTwoSameDigits(int num) {
-	int a = num / 100;
-	int b = (num / 10) % 10;
-	int c = num % 10;
-	if (a == b && b != c || a == c && b != c || b == c && a != c) {
-		return true;
-	}
-	return false;
-}
-
 
 int main() {
+	//Підрахувати кількість цілих чисел у діапазоні від 100 до 999, у яких усі цифри різні.
 	int count = 0;
 	for (int i = 100; i < 1000; i++) {
-		if (hasTwoSameDigits(i)) {
+		int a = i / 100;
+		int b = (i / 10) % 10;
+		int c = i % 10;
+		if (a != b && a != c && b != c) {
 			count++;
 		}
 	}
