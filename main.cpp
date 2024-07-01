eed5c1f31e49f7122ac5605249591e3bc8d973ee
Bashmeg <superbashmeg@gmail.com>
2024-07-01 18:29:09 +0300

    task 1 done

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
-
+	int n;
+	cout << "Enter a number: ";
+	cin >> n;
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << i << ' ';
+	}
+	cout << endl;
 	return 0;
 }
