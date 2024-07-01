06222cf8808993f26d40b12e9399f1440cba7dff
Bashmeg <superbashmeg@gmail.com>
2024-07-01 19:13:45 +0300

    task 7 done

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 
 
 int main() {
-	int a;
-	cout << "Enter a number: ";
-	cin >> a;
-	cout << "All numbers that " << a << " is divided by without a remainder: ";
-	for (int i = 1; i <= a; i++) {
-		if (a % i == 0) {
+	int a, b;
+	cout << "Enter two numbers: ";
+	cin >> a >> b;
+	cout << "All numbers that both entered numbers are divided without a remainder: ";
+	for (int i = 1; i <= a && i <= b; i++) {
+		if (a % i == 0 && b % i == 0) {
 			cout << i << " ";
 		}
 	}
