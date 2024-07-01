97f7cf00d4333e5529553535e2e48f199ff3147a
Bashmeg <superbashmeg@gmail.com>
2024-07-01 18:36:44 +0300

    task 3 fixed

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 int main() {
-	int num;
+	int start;
+	int end;
 	int sum = 0;
-	do
-	{
-		cout << "Enter a number: ";
-		cin >> num;
-		sum += num;
-	} while (num != 0);
-	cout << "The sum is: " << sum << endl;
+	cout << "Enter the start of range: ";
+	cin >> start;
+	cout << "Enter the end of range: ";
+	cin >> end;
+	for (int i = start + 1; i < end; i++) {
+		sum += i;
+	}
+	cout << "Sum of numbers between " << start << " and " << end << " is " << sum << endl;
 	return 0;
 }
