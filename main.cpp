b67bc415deada229f2b85ae00e94154ee26080b8
Bashmeg <superbashmeg@gmail.com>
2024-07-01 20:28:04 +0300

    task 2 overdone

--- main.cpp
+++ main.cpp
@@ -1,17 +1,69 @@
 #include "struct.h"
 
+void display_points(point* pt1, point* pt2)
+{
+	char ul = 187, lr = 188, h = 205, v = 186;
+	int pt1_x = *pt1->get_pos_x();
+	int pt1_y = *pt1->get_pos_y();
+	int pt2_x = *pt2->get_pos_x();
+	int pt2_y = *pt2->get_pos_y();
+	int max_x = max(pt1_x, pt2_x);
+	int max_y = max(pt1_y, pt2_y);
+	cout << pt1_x << " " << pt1_y << endl;
+	cout << pt2_x << " " << pt2_y << endl;
+	cout << max_x << " " << max_y << endl;
+	for (size_t i = 0; i < max_x+1; i++)
+	{
+		if (i == 0)
+			cout << h;
+		else if (i == max_x)
+			cout << ul;
+		else
+			cout << h;
+	}
+	cout << endl;
+	for (size_t i = 0; i < max_y; i++)
+	{
+		for (size_t j = 0; j < max_x; j++)
+		{
+			if (j == pt1_x-1 && i == pt1_y-1)
+				cout << "1";
+			else if (j == pt2_x-1 && i == pt2_y-1)
+				cout << "2";
+			else
+				cout << " ";
+		}
+		cout << v;
+		cout << endl;
+	}
+	for (size_t i = 0; i < max_x+1; i++)
+	{
+		if (i == 0)
+			cout << h;
+		else if (i == max_x)
+			cout << lr;
+		else
+			cout << h;
+	}
+}
+
+double get_distance(point* pt1, point* pt2)
+{
+	int x = *pt1->get_pos_x() - *pt2->get_pos_x();
+	int y = *pt1->get_pos_y() - *pt2->get_pos_y();
+	return sqrt(x * x + y * y);
+}
+
 int main()
 {
-    my_Rectangle* rectangle = new my_Rectangle;
-	int* height = new int(5);
-	int* width = new int(10);
-	rectangle->set_height(height);
-	rectangle->set_width(width);
-	rectangle->display();
-	rectangle->move(5, 5);
-	rectangle->moveWithDisplay(1, 1);
-	rectangle->moveWithDisplay(1, -1);
-	
+	point* pt1 = new point;
+	pt1->move(3, 3);
+	point* pt2 = new point;
+	pt2->move(15, 2);
+	display_points(pt1, pt2);
 	cout << endl;
-    delete rectangle;
+	cout << "Distance between points: " << get_distance(pt1, pt2) << endl;
+	
+    delete pt1;
+	delete pt2;
 }
--- struct.h
+++ struct.h
@@ -5,11 +5,9 @@
 using namespace std;
 
 
-struct my_Rectangle
+struct point
 {
 private:
-    int* height = nullptr;
-    int* width = nullptr;
 	int* pos_x = nullptr;
 	int* pos_y = nullptr;
 
@@ -29,24 +27,12 @@ private:
         SetConsoleTextAttribute(hConsole, 7);
     }
 public:
-	my_Rectangle() //default 
+	point() //default 
     {
         show_create_log();
     }
 
     //getters
-    int* get_height() {
-        if (this->height != nullptr)
-            return this->height;
-        else
-            return new int{ 0 };
-    }
-    int* get_width() {
-		if (this->width != nullptr)
-			return this->width;
-		else
-			return new int{ 0 };
-	}
     int* get_pos_x() {
 		if (this->pos_x != nullptr)
 			return this->pos_x;
@@ -62,14 +48,6 @@ public:
 
 
     //setters
-    void set_height(int* height) {
-        if (height != nullptr)
-            this->height = height;
-    }
-	void set_width(int* width) {
-		if (width != nullptr)
-		    this->width = width;
-	}
 	void set_pos_x(int* pos_x) {
 		if (pos_x != nullptr)
 		    this->pos_x = pos_x;
@@ -79,48 +57,6 @@ public:
 			this->pos_y = pos_y;
     }
 
-
-	void display() {
-		char upper_left = 201;
-		char upper_right = 187;
-		char lower_left = 200;
-		char lower_right = 188;
-		char horizontal = 205;
-		char vertical = 186;
-
-		if (pos_x == nullptr) pos_x = new int{ 0 };
-		if (pos_y == nullptr) pos_y = new int{ 0 };
-	
-		for (size_t ey = 0; ey < *pos_y; ey++) {
-			cout << endl;
-		}
-        for (size_t i = 0; i < *height; i++)
-		{
-			for (size_t ex = 0; ex < *pos_x; ex++) {
-				cout << ' ';
-			}
-			for (size_t j = 0; j < *width; j++)
-			{
-				if (i == 0 && j == 0)
-					cout << upper_left;
-				else if (i == 0 && j == *width - 1)
-					cout << upper_right;
-				else if (i == *height - 1 && j == 0)
-					cout << lower_left;
-				else if (i == *height - 1 && j == *width - 1)
-					cout << lower_right;
-				else if (i == 0 || i == *height - 1)
-					cout << horizontal;
-				else if (j == 0 || j == *width - 1)
-					cout << vertical;
-				else
-					cout << " ";
-			}
-			cout << endl;
-
-        }
-	}
-
 	void move(int x, int y) {
 		if (pos_x == nullptr) pos_x = new int{ 0 };
 		if (pos_y == nullptr) pos_y = new int{ 0 };
@@ -128,31 +64,15 @@ public:
 		*pos_y += y;
 	}
 
-	void moveWithDisplay(int x, int y) {
-		if (pos_x == nullptr) pos_x = new int{ 0 };
-		if (pos_y == nullptr) pos_y = new int{ 0 };
-		*pos_x += x;
-		*pos_y += y;
-		for (size_t i = 0; i < *width + *pos_x * 2; i++)
-		{
-			cout << "_";
-		}
-		display();
-	}
-
     void about()
     {
-        cout << "Height: " << *this->get_height() << " inches" << endl;
-		cout << "Width: " << *this->get_width() << " inches" << endl;
 		cout << "Position X: " << *this->get_pos_x() << " inches" << endl;
 		cout << "Position Y: " << *this->get_pos_y() << " inches" << endl;
     }
 
-    ~my_Rectangle()
+    ~point()
     {
         show_destroy_log();
-        if (height != nullptr) delete height;
-		if (width != nullptr) delete width;
 		if (pos_x != nullptr) delete pos_x;
 		if (pos_y != nullptr) delete pos_y;
     }
