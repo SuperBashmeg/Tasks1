c48fd4823e032c36547d1245d150da7ea543b873
Bashmeg <superbashmeg@gmail.com>
2024-07-01 18:31:17 +0300

    task 2 done

--- main.cpp
+++ main.cpp
@@ -2,12 +2,20 @@
 using namespace std;
 
 int main() {
-	int n;
-	cout << "Enter a number: ";
-	cin >> n;
-	for (size_t i = 0; i < n; i++)
+	int start;
+	cout << "Enter a start of range: ";
+	cin >> start;
+	int end;
+	cout << "Enter an end of range: ";
+	cin >> end;
+	if (end < start) {
+		int temp = start;
+		start = end;
+		end = temp;
+	}
+	for (size_t i = start+1; i < end; i++)
 	{
-		cout << i << ' ';
+		cout << i << " ";
 	}
 	cout << endl;
 	return 0;
