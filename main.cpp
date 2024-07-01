bd596f524c7e516416ef6170a74e1394eb698040
Bashmeg <superbashmeg@gmail.com>
2024-07-01 19:12:51 +0300

    task 6 done

--- main.cpp
+++ main.cpp
@@ -5,22 +5,12 @@ int main() {
 	int a;
 	cout << "Enter a number: ";
 	cin >> a;
-	if (a == 0) {
-		cout << "The cube of the sum of the digits of this number is equal to " << a * a << endl;
-	}
-	else {
-		int sum = 0;
-		int temp = a;
-		while (temp != 0) {
-			sum += temp % 10;
-			temp /= 10;
-		}
-		if (sum * sum * sum == a * a) {
-			cout << "The cube of the sum of the digits of this number is equal to " << a * a << endl;
-		}
-		else {
-			cout << "The cube of the sum of the digits of this number is not equal to " << a * a << endl;
+	cout << "All numbers that " << a << " is divided by without a remainder: ";
+	for (int i = 1; i <= a; i++) {
+		if (a % i == 0) {
+			cout << i << " ";
 		}
 	}
+	cout << endl;
 	return 0;
 }
