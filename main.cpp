867226c48f26da9c6bc8105f4719205aec0cc26d
Bashmeg <superbashmeg@gmail.com>
2024-11-26 19:01:06 +0200

    done!

--- main.cpp
+++ main.cpp
@@ -8,66 +8,55 @@ void fillArray(int rows, int cols, int arr[][100], int minVal, int maxVal) {
     }
 }
 
-int sumArray(int rows, int cols, int arr[][100]) {
-    int sum = 0;
+void calculateSums(int rows, int cols, int arr[][100], int rowSums[], int colSums[], int& totalSum) {
+    totalSum = 0;
+    for (int i = 0; i < rows; ++i) rowSums[i] = 0;
+    for (int j = 0; j < cols; ++j) colSums[j] = 0;
+
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            sum += arr[i][j];
+            rowSums[i] += arr[i][j];
+            colSums[j] += arr[i][j];
+            totalSum += arr[i][j];
         }
     }
-    return sum;
 }
 
-int findMin(int rows, int cols, int arr[][100]) {
-    int minVal = arr[0][0];
+void displayArrayWithSums(int rows, int cols, int arr[][100], int rowSums[], int colSums[], int totalSum) {
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            if (arr[i][j] < minVal) {
-                minVal = arr[i][j];
-            }
+            cout << arr[i][j] << "\t";
         }
+        cout << "| " << rowSums[i] << endl;
     }
-    return minVal;
-}
 
-int findMax(int rows, int cols, int arr[][100]) {
-    int maxVal = arr[0][0];
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            if (arr[i][j] > maxVal) {
-                maxVal = arr[i][j];
-            }
-        }
+    for (int j = 0; j < cols; ++j) {
+        cout << "--------";
     }
-    return maxVal;
+    cout << endl;
+
+    for (int j = 0; j < cols; ++j) {
+        cout << colSums[j] << "\t";
+    }
+    cout << "| " << totalSum << endl;
 }
 
 int main() {
     srand(time(0));
 
-    const int rows = 4;
-    const int cols = 5;
+    const int rows = 3;
+    const int cols = 4;
     int arr[100][100];
+    int rowSums[100];
+    int colSums[100];
+    int totalSum = 0;
 
-    fillArray(rows, cols, arr, -50, 50);
+    fillArray(rows, cols, arr, 0, 9);
 
-    cout << "Array:\n";
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            cout << arr[i][j] << "\t";
-        }
-        cout << endl;
-    }
+    calculateSums(rows, cols, arr, rowSums, colSums, totalSum);
 
-    int sum = sumArray(rows, cols, arr);
-    int minVal = findMin(rows, cols, arr);
-    int maxVal = findMax(rows, cols, arr);
-    double average = static_cast<double>(sum) / (rows * cols);
-
-    cout << "\nSum: " << sum << endl;
-    cout << "Avarage: " << average << endl;
-    cout << "Min:" << minVal << endl;
-    cout << "Max: " << maxVal << endl;
+    cout << "Array with sums:\n";
+    displayArrayWithSums(rows, cols, arr, rowSums, colSums, totalSum);
 
     return 0;
 }
